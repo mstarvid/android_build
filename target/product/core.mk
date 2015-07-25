ba5eef5283ba1bc9ab83384f8cca703101e62d98
mrksutter <marks@morningstarvideo.com>
2015-07-25 02:32:53 -0500

    Update core.mk

--- target/product/core.mk
+++ target/product/core.mk
@@ -22,17 +22,12 @@
 
 PRODUCT_PACKAGES += \
     BasicDreams \
-    Browser \
-    Calculator \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
-    Contacts \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
     Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
